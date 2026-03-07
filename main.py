1279bd7212d2e3692c25c1d04ecf95322220351b
Ангелина Петрова <linalleks@gmail.com>
2026-03-07 15:42:38 +0700

    divided funcs to parts

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import requests
 from decouple import config
 from terminaltables import AsciiTable
 
+PROG_LANGS = ["Python", "Java", "Javascript", "Go", "C++", "C#", "PHP", "Ruby"]
+
 
 def predict_salary(salary_from, salary_to):
     if not salary_from:
@@ -33,24 +35,40 @@ def predict_rub_salary_sj(vacancy):
 
 
 def print_statistics_table(title, salary_statistics_in_prog_langs):
-    TABLE_DATA = [
+    table_data = [
         ['Язык программирования', 'Вакансий найдено', 'Вакансий обработано', 'Средняя зарплата']
     ]
 
     for prog_lang, salary_statistics in salary_statistics_in_prog_langs.items():
-        TABLE_DATA.append([prog_lang] + list(salary_statistics.values()))
+        table_data.append([prog_lang] + list(salary_statistics.values()))
 
-    table_instance = AsciiTable(TABLE_DATA, title)
+    table_instance = AsciiTable(table_data, title)
     print(table_instance.table)
     print()
 
 
-def get_salary_statistics_hh(prog_langs):
+def calc_salary_statistics(all_vacancies, vacancies_found, func_predict_salary):
+    vacancies_processed = 0
+    average_salary = 0
+    for vacancy in all_vacancies:
+        salary = func_predict_salary(vacancy)
+        if salary:
+            vacancies_processed += 1
+            average_salary += salary
+
+    if vacancies_processed:
+        average_salary = int(average_salary / vacancies_processed)
+
+    return {'vacancies_found': vacancies_found,
+            'vacancies_processed': vacancies_processed,
+            'average_salary': average_salary}
+
+
+def get_salary_statistics_hh():
     salary_statistics_in_prog_langs = {}
-    for prog_lang in prog_langs:
+    for prog_lang in PROG_LANGS:
         page = 0
         all_vacancies = []
-        salary_statistics = {}
         while True:
             params = {
                 'professional_role': 96,
@@ -67,35 +85,21 @@ def get_salary_statistics_hh(prog_langs):
             total_pages = vacancies_page.get("pages")
             page += 1
             if total_pages == page:
-                salary_statistics["vacancies_found"] = vacancies_page["found"]
-                salary_statistics["vacancies_processed"] = 0
-                salary_statistics["average_salary"] = 0
                 break
+        salary_statistics_in_prog_langs[prog_lang] = calc_salary_statistics(
+            all_vacancies, vacancies_page["found"], predict_rub_salary_hh)
 
-        for vacancy in all_vacancies:
-            salary = predict_rub_salary_hh(vacancy)
-            if salary:
-                salary_statistics["vacancies_processed"] += 1
-                salary_statistics["average_salary"] += salary
-
-        if salary_statistics["vacancies_processed"] > 0:
-            salary_statistics["average_salary"] = int(
-                salary_statistics["average_salary"] / salary_statistics["vacancies_processed"])
-
-        salary_statistics_in_prog_langs[prog_lang] = salary_statistics
-
-    print_statistics_table('HeadHunter Moscow', salary_statistics_in_prog_langs)
+    return salary_statistics_in_prog_langs
 
 
-def get_salary_statistics_sj(prog_langs):
+def get_salary_statistics_sj(secret_key):
     salary_statistics_in_prog_langs = {}
     headers = {
-        'X-Api-App-Id': config('SJ_SECRET_KEY')
+        'X-Api-App-Id': secret_key
     }
-    for prog_lang in prog_langs:
+    for prog_lang in PROG_LANGS:
         page = 0
         all_vacancies = []
-        salary_statistics = {}
         while True:
             params = {
                 'town': 4,
@@ -112,30 +116,16 @@ def get_salary_statistics_sj(prog_langs):
             if vacancies_page["more"]:
                 page += 1
             else:
-                salary_statistics["vacancies_found"] = vacancies_page["total"]
-                salary_statistics["vacancies_processed"] = 0
-                salary_statistics["average_salary"] = 0
                 break
+        salary_statistics_in_prog_langs[prog_lang] = calc_salary_statistics(
+            all_vacancies, vacancies_page["total"], predict_rub_salary_sj)
 
-        for vacancy in all_vacancies:
-            salary = predict_rub_salary_sj(vacancy)
-            if salary:
-                salary_statistics["vacancies_processed"] += 1
-                salary_statistics["average_salary"] += salary
-
-        if salary_statistics["vacancies_processed"] > 0:
-            salary_statistics["average_salary"] = int(
-                salary_statistics["average_salary"] / salary_statistics["vacancies_processed"])
-
-        salary_statistics_in_prog_langs[prog_lang] = salary_statistics
-
-    print_statistics_table('SuperJob Moscow', salary_statistics_in_prog_langs)
+    return salary_statistics_in_prog_langs
 
 
 def main():
-    prog_langs = ["Python", "Java", "Javascript", "Go", "C++", "C#", "PHP", "Ruby"]
-    get_salary_statistics_hh(prog_langs)
-    get_salary_statistics_sj(prog_langs)
+    print_statistics_table('HeadHunter Moscow', get_salary_statistics_hh())
+    print_statistics_table('SuperJob Moscow', get_salary_statistics_sj(config('SJ_SECRET_KEY')))
 
 
 if __name__ == '__main__':
